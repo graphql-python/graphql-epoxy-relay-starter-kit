58c92d210f410eae581cb37e97859a1a8006b960
Jake <me@jh.gg>
2015-10-28 14:20:36 -0400

    Update relay starter kit to use latest epoxy. And add some pagination in App.js for playing with.

--- js/components/App.js
+++ js/components/App.js
@@ -3,32 +3,53 @@ import Relay from 'react-relay';
 
 class App extends React.Component {
   render() {
+    const { viewer, relay } = this.props;
+
     return (
       <div>
-        <h1>Welcome, {this.props.viewer.name}</h1>
-        <h1>Widget list</h1>
+        <h1>Welcome, {viewer.name}</h1>
+
+        <h1>Widget list (current count: {relay.variables.count})</h1>
         <ul>
-          {this.props.viewer.widgets.edges.map(edge =>
-            <li>{edge.node.name} (ID: {edge.node.id})</li>
+          {viewer.widgets.edges.map(edge =>
+              <li key={edge.node.id}>{edge.node.name} (ID: {edge.node.id})</li>
           )}
         </ul>
+        {viewer.widgets.pageInfo.hasNextPage && <button onClick={() => {
+          relay.setVariables({
+            count: relay.variables.count + 1
+          })
+        }}>Show me more</button>}
+        {relay.variables.count > 1 && <button onClick={() => {
+          relay.setVariables({
+            count: Math.max(1, relay.variables.count - 1)
+          })
+        }}>Show me less
+        </button>}
       </div>
     );
   }
 }
 
 export default Relay.createContainer(App, {
+  initialVariables: {
+    count: 1
+  },
   fragments: {
     viewer: () => Relay.QL`
       fragment on User {
         name
-        widgets(first: 10) {
+        widgets(first: $count) {
           edges {
             node {
               id,
               name,
             },
-          },
+          }
+          pageInfo {
+            hasPreviousPage
+            hasNextPage
+          }
         },
       }
     `,
